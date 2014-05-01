6f346dd43e6156582a189092a889785c94737108
Michael Schubmehl <michael.schubmehl@gmail.com>
2014-05-01 00:08:11 -0500

    manual: allow manual insertion of orders for testing

--- me/manual.py
+++ me/manual.py
@@ -3,17 +3,18 @@ import sys
 import time
 
 HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
 
 # Create a socket (SOCK_STREAM means a TCP socket)
-for i in xrange(10):
+while True:
+    print "Message: ",
+    data = sys.stdin.readline()
+
     try:
+        # Connect to server and send data
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((HOST, PORT))
-
-        # Connect to server and send data
-        sock.sendall(data + str(i) + "\n")
-        print "Sent:     {}".format(data)
+        sock.sendall(data + "\n")
+        print "Sent: ", data
     finally:
         sock.close()
     time.sleep(1)
