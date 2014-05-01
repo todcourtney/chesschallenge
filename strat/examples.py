ebb16692a3a9f8fb0ef99777131d90f5037a9a61
Michael Schubmehl <michael.schubmehl@gmail.com>
2014-05-01 00:12:10 -0500

    examples: add example executor (work in progress, since matching engine doesn't respond to orders yet)

--- strat/examples.py
+++ strat/examples.py
@@ -1,4 +1,5 @@
 import strat
+from me import Order ## TODO: will want to factor out actual matching engine code
 from ChessBoard import ChessBoard
 
 class SimpleChessModel(strat.Model):
@@ -24,3 +25,49 @@ class SimpleChessModel(strat.Model):
         if p < 0.01: p = 0.01
         if p > 0.99: p = 0.99
         return p
+
+class SimpleExecutor(strat.Executor):
+    def __init__(self):
+        self.model = SimpleChessModel()
+        self.liveOrders = []
+
+    def onChessMove(self,move):
+        ## pass through to model
+        self.model.onChessMove(move)
+
+        ## decide my bid price and ask price
+        fairPrice = self.model.fairProbabilityEstimate()*100
+        buyPrice  = round(fairPrice-2)
+        sellPrice = round(fairPrice+2)
+        print "prices: ", fairPrice, buyPrice, sellPrice
+
+        ## keep desired quantity showing at indicated price
+        desiredQty = 10
+        alreadyHaveBuy  = False
+        alreadyHaveSell = False
+        for o in self.liveOrders:
+            if o.side == Order.BUY:
+                if o.price == buyPrice:
+                    alreadyHaveBuy = True
+                    if o.qty < desiredQty:
+                        self.sendOrder(desiredQty-o.qty, Order.BUY, buyPrice)
+                elif o.price > buyPrice:
+                    self.cancelOrder(o.oid)
+            else:
+                if o.price == sellPrice:
+                    alreadyHaveSell = True
+                    if o.qty < desiredQty:
+                        self.sendOrder(desiredQty-o.qty, Order.SELL, sellPrice)
+                elif o.price < sellPrice:
+                    self.cancelOrder(o.oid)
+
+        if not alreadyHaveBuy : self.sendOrder(desiredQty, Order.BUY , buyPrice)
+        if not alreadyHaveSell: self.sendOrder(desiredQty, Order.SELL, sellPrice)
+
+if __name__ == "__main__":
+    import sys
+    x = SimpleExecutor()
+    while True:
+        print "Move: ",
+        move = sys.stdin.readline().rstrip()
+        x.onChessMove(move)
