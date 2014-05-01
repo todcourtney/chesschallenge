921183990334ae22123f52d4c78356150a953fb5
Michael Schubmehl <michael.schubmehl@gmail.com>
2014-05-01 00:34:01 -0500

    gui: add chess game playing example

--- gui/test.py
+++ gui/test.py
@@ -18,10 +18,9 @@ try:
     stdscr.addstr("Pretty text\nSecond line")
     stdscr.refresh()
 
-    boardPad.addstr(chess.prettyBoardString())
-    boardPad.refresh(0,0, 5,45, 35,75)
-
     ask = 5
+
+    moves = "e4 c5 Nf3 e6 d4 cxd4 Nxd4 a6 Bd3 Nf6 O-O Qc7 Qe2 d6 c4 g6 Nc3 Bg7 Nf3 O-O Bf4 Nc6 Rac1 e5 Bg5 h6 Be3 Bg4 Nd5 Qd8 h3 Nxd5 cxd5 Nd4 Bxd4 Bxf3 Qxf3 exd4 Rc4 Rc8 Rfc1 Rxc4 Rxc4 h5 Qd1 Be5 Qc1 Qf6 Rc7 Rb8 a4 Kg7 b4 h4 Kf1 Bf4 Qd1 Qd8 Rc4 Rc8 a5 Rxc4 Bxc4 Qf6 Be2 Be5 Bf3 Qd8 Qc2 b6 axb6 Qxb6 Qc4 d3".split()
     
     while True:
         ask = random.randrange(max(ask-1,1), min(ask+1,9)+1)
@@ -36,10 +35,17 @@ try:
         #  Displays a section of the pad in the middle of the screen
         ladderPad.refresh(0,0, 5,5, 20,40)
 
+        boardPad.addstr(0,0,chess.prettyBoardString())
+        boardPad.refresh(0,0, 5,45, 35,75)
+
         # now get input
         c = stdscr.getch()
         if c == ord('q'):
             break
+
+        if len(moves):
+            m = moves.pop(0)
+            chess.addTextMove(m)
 
 finally:
     curses.nocbreak()
