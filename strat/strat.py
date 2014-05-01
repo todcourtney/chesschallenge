a1e0f087324ea37d2d16f13a604badae1db816de
Michael Schubmehl <michael.schubmehl@gmail.com>
2014-05-01 00:09:07 -0500

    strat: implement sendOrder

--- strat/strat.py
+++ strat/strat.py
@@ -1,4 +1,5 @@
-
+from me import Order ## factor out
+import socket
 
 class MarketListener:
     def onMarketActivity(self,book,trade):
@@ -20,13 +21,16 @@ class Executor(MarketListener, ChessListener):
         pass
 
     def sendOrder(self,qty,side,price):
+        data = "A,%d,%s,%d\n" % (qty, "B" if side == Order.BUY else "S", price)
+        print data
+        HOST, PORT = "localhost", 9999
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sock.connect((HOST, PORT))
+            sock.sendall(data)
+        finally:
+            sock.close()
+
+    def cancelOrder(self,oid):
         pass
-
-
-
-
-
-
-
-
 
