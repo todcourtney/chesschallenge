8bf28dce2304f03bdba41fb6efc63b751adf112d
Michael Schubmehl <mschubmehl@jumptrading.com>
2014-05-02 16:08:19 -0500

    me: add basic chess replay multicast too

--- me/me.py
+++ me/me.py
@@ -9,6 +9,10 @@ from gateway import AddOrderMessage, CancelOrderMessage
 
 import feed
 
+chessgame = {"moves":"e4 c5 Nf3 e6 d4 cxd4 Nxd4 a6 Bd3 Nf6 O-O Qc7 Qe2 d6 c4 g6 Nc3 Bg7 Nf3 O-O Bf4 Nc6 Rac1 e5 Bg5 h6 Be3 Bg4 Nd5 Qd8 h3 Nxd5 cxd5 Nd4 Bxd4 Bxf3 Qxf3 exd4 Rc4 Rc8 Rfc1 Rxc4 Rxc4 h5 Qd1 Be5 Qc1 Qf6 Rc7 Rb8 a4 Kg7 b4 h4 Kf1 Bf4 Qd1 Qd8 Rc4 Rc8 a5 Rxc4 Bxc4 Qf6 Be2 Be5 Bf3 Qd8 Qc2 b6 axb6 Qxb6 Qc4 d3".split(" "), "result":"1/2-1/2"}
+speed = 2
+nextMove = time.time() + speed
+
 if __name__ == "__main__":
     gateways = gateway.GatewayCollection()
     f = feed.Feed()
@@ -16,6 +20,16 @@ if __name__ == "__main__":
 
     newoid = 1
     while True:
+        if nextMove is not None and time.time() > nextMove:
+            if len(chessgame['moves']):
+                f.send("M," + chessgame['moves'].pop(0))
+                nextMove = time.time() + speed
+            else:
+                f.send("R," + chessgame['result'])
+                nextMove = None
+                ## TODO: load new game
+                pass
+
         m, g = gateways.getIncomingMessage()
         if m is None:
             time.sleep(0.1)
