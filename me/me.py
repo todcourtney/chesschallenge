0d600257c4ea009b2473c312e46e29be007e5630
Michael Schubmehl <michael.schubmehl@gmail.com>
2014-05-02 19:58:11 -0500

    me: send new-game and then repeat same game for now

--- me/me.py
+++ me/me.py
@@ -20,15 +20,16 @@ if __name__ == "__main__":
 
     newoid = 1
     while True:
-        if nextMove is not None and time.time() > nextMove:
+        if time.time() > nextMove:
             if len(chessgame['moves']):
                 f.send("M," + chessgame['moves'].pop(0))
-                nextMove = time.time() + speed
             else:
                 f.send("R," + chessgame['result'])
-                nextMove = None
+                f.send("N")
+                chessgame = {"moves":"e4 c5 Nf3 e6 d4 cxd4 Nxd4 a6 Bd3 Nf6 O-O Qc7 Qe2 d6 c4 g6 Nc3 Bg7 Nf3 O-O Bf4 Nc6 Rac1 e5 Bg5 h6 Be3 Bg4 Nd5 Qd8 h3 Nxd5 cxd5 Nd4 Bxd4 Bxf3 Qxf3 exd4 Rc4 Rc8 Rfc1 Rxc4 Rxc4 h5 Qd1 Be5 Qc1 Qf6 Rc7 Rb8 a4 Kg7 b4 h4 Kf1 Bf4 Qd1 Qd8 Rc4 Rc8 a5 Rxc4 Bxc4 Qf6 Be2 Be5 Bf3 Qd8 Qc2 b6 axb6 Qxb6 Qc4 d3".split(" "), "result":"1/2-1/2"}
                 ## TODO: load new game
                 pass
+            nextMove = time.time() + speed
 
         m, g = gateways.getIncomingMessage()
         if m is None:
