3677bf579845c793173168302cf9e1a739bab643
Michael Schubmehl <michael.schubmehl@gmail.com>
2014-05-02 21:55:08 -0500

    me: factor out recovery code

--- me/me.py
+++ me/me.py
@@ -16,11 +16,43 @@ n = 0
 
 nextRecovery = time.time() + 5
 
+class RecoveryBuilder:
+    def __init__(self, b, f, freq=5):
+        self.b = b
+        self.f = f
+        self.freq = freq
+        self.reset()
+
+    def reset(self):
+        self.nextTime = time.time() + self.freq
+
+    def sendIfNeeded(self):
+        ## send recovery message every so often (may be very large)
+        if time.time() > nextRecovery:
+            recoveryMessageList = []
+            recoveryMessage = None
+            messages = self.b.getStateForRecoveryMessage()
+            for r in messages:
+                rs = ",".join(str(field) for field in r)
+                recoveryMessageExpanded = recoveryMessage + ";" + rs if recoveryMessage is not None else rs
+                if len(recoveryMessageExpanded) + 15 < feed.Feed.MAX_SIZE:
+                    recoveryMessage = recoveryMessageExpanded
+                else:
+                    recoveryMessageList.append(recoveryMessage)
+                    recoveryMessage = rs
+            if recoveryMessage is not None:
+                recoveryMessageList.append(recoveryMessage)
+            recoveryMessageList = ["BS"] + ["BR," + rm for rm in recoveryMessageList] + ["BE"]
+            for rm in recoveryMessageList:
+                self.f.send(rm)
+            self.reset()
+
 if __name__ == "__main__":
     gateways = gateway.GatewayCollection()
     f = feed.Feed()
     f.send("N")
     b = Book()
+    r = RecoveryBuilder(b,f)
 
     newoid = 1
     while True:
@@ -38,30 +70,13 @@ if __name__ == "__main__":
                 chessgame = {"moves":"e4 c5 Nf3 e6 d4 cxd4 Nxd4 a6 Bd3 Nf6 O-O Qc7 Qe2 d6 c4 g6 Nc3 Bg7 Nf3 O-O Bf4 Nc6 Rac1 e5 Bg5 h6 Be3 Bg4 Nd5 Qd8 h3 Nxd5 cxd5 Nd4 Bxd4 Bxf3 Qxf3 exd4 Rc4 Rc8 Rfc1 Rxc4 Rxc4 h5 Qd1 Be5 Qc1 Qf6 Rc7 Rb8 a4 Kg7 b4 h4 Kf1 Bf4 Qd1 Qd8 Rc4 Rc8 a5 Rxc4 Bxc4 Qf6 Be2 Be5 Bf3 Qd8 Qc2 b6 axb6 Qxb6 Qc4 d3".split(" "), "result":"1/2-1/2"}
                 n = 0
                 b = Book() ## TODO: should send out cancels for everybody's orders?
+                r = RecoveryBuilder(b,f)
                 ## TODO: need to clear gateway queues
                 pass
             nextMove = time.time() + speed
 
-        ## send recovery message every so often (may be very large)
-        if time.time() > nextRecovery:
-            recoveryMessageList = []
-            recoveryMessage = None
-            messages = b.getStateForRecoveryMessage()
-            for r in messages:
-                rs = ",".join(str(field) for field in r)
-                recoveryMessageExpanded = recoveryMessage + ";" + rs if recoveryMessage is not None else rs
-                if len(recoveryMessageExpanded) + 15 < feed.Feed.MAX_SIZE:
-                    recoveryMessage = recoveryMessageExpanded
-                else:
-                    recoveryMessageList.append(recoveryMessage)
-                    recoveryMessage = rs
-            if recoveryMessage is not None:
-                recoveryMessageList.append(recoveryMessage)
-            recoveryMessageList = ["BS"] + ["BR," + rm for rm in recoveryMessageList] + ["BE"]
-            print "recovery:"
-            print "\n".join(recoveryMessageList)
-            for rm in recoveryMessageList: f.send(rm)
-            nextRecovery = time.time() + 5
+        ## send recovery messages if needed
+        r.sendIfNeeded()
 
         m, g = gateways.getIncomingMessage()
         if m is None:
