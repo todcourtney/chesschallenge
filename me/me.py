eee0b036d9fca9db03fa7fd179cdb75f40618b4d
Michael Schubmehl <michael.schubmehl@gmail.com>
2014-05-02 21:40:18 -0500

    me: send empty recovery when no orders; speed up games but delay between

--- me/me.py
+++ me/me.py
@@ -10,7 +10,7 @@ from gateway import AddOrderMessage, CancelOrderMessage
 import feed
 
 chessgame = {"moves":"e4 c5 Nf3 e6 d4 cxd4 Nxd4 a6 Bd3 Nf6 O-O Qc7 Qe2 d6 c4 g6 Nc3 Bg7 Nf3 O-O Bf4 Nc6 Rac1 e5 Bg5 h6 Be3 Bg4 Nd5 Qd8 h3 Nxd5 cxd5 Nd4 Bxd4 Bxf3 Qxf3 exd4 Rc4 Rc8 Rfc1 Rxc4 Rxc4 h5 Qd1 Be5 Qc1 Qf6 Rc7 Rb8 a4 Kg7 b4 h4 Kf1 Bf4 Qd1 Qd8 Rc4 Rc8 a5 Rxc4 Bxc4 Qf6 Be2 Be5 Bf3 Qd8 Qc2 b6 axb6 Qxb6 Qc4 d3".split(" "), "result":"1/2-1/2"}
-speed = 2
+speed = 0.5
 nextMove = time.time() + speed
 n = 0
 
@@ -32,6 +32,7 @@ if __name__ == "__main__":
                 n += 1
             else:
                 f.send("R," + chessgame['result'])
+                time.sleep(10)
                 f.send("N")
                 ## TODO: load new game
                 chessgame = {"moves":"e4 c5 Nf3 e6 d4 cxd4 Nxd4 a6 Bd3 Nf6 O-O Qc7 Qe2 d6 c4 g6 Nc3 Bg7 Nf3 O-O Bf4 Nc6 Rac1 e5 Bg5 h6 Be3 Bg4 Nd5 Qd8 h3 Nxd5 cxd5 Nd4 Bxd4 Bxf3 Qxf3 exd4 Rc4 Rc8 Rfc1 Rxc4 Rxc4 h5 Qd1 Be5 Qc1 Qf6 Rc7 Rb8 a4 Kg7 b4 h4 Kf1 Bf4 Qd1 Qd8 Rc4 Rc8 a5 Rxc4 Bxc4 Qf6 Be2 Be5 Bf3 Qd8 Qc2 b6 axb6 Qxb6 Qc4 d3".split(" "), "result":"1/2-1/2"}
@@ -56,11 +57,10 @@ if __name__ == "__main__":
                     recoveryMessage = rs
             if recoveryMessage is not None:
                 recoveryMessageList.append(recoveryMessage)
-            if len(recoveryMessageList):
-                recoveryMessageList = ["BS"] + ["BR," + rm for rm in recoveryMessageList] + ["BE"]
-                print "recovery:"
-                print "\n".join(recoveryMessageList)
-                for rm in recoveryMessageList: f.send(rm)
+            recoveryMessageList = ["BS"] + ["BR," + rm for rm in recoveryMessageList] + ["BE"]
+            print "recovery:"
+            print "\n".join(recoveryMessageList)
+            for rm in recoveryMessageList: f.send(rm)
             nextRecovery = time.time() + 5
 
         m, g = gateways.getIncomingMessage()
