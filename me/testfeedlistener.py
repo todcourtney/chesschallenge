7a8447e3e7c5d2d96c32aaeefe0b23f2ae7fa6b3
Michael Schubmehl <michael.schubmehl@gmail.com>
2014-05-05 22:36:02 -0500

    testfeedlistener: code for both threading and non-threading modes

--- me/testfeedlistener.py
+++ me/testfeedlistener.py
@@ -1,6 +1,19 @@
 import feed
+import time
 
-f = feed.Feed(send=False, receive=True)
-while True:
-  msg, seq, drop, m = f.recv()
-  print "%08d %1s: %s" % (seq, "*" if drop else " ", m)
+useThread = True
+
+class FeedPrinter(feed.Listener):
+  def onFeedMessage(self, rawMessage, seq, drop, message):
+    print "%08d %1s: %s" % (seq, "*" if drop else " ", message)
+
+if useThread:
+  L = FeedPrinter()
+  f = feed.Feed(send=False, receive=True, thread=True, listeners=[L])
+  while True:
+    time.sleep(1)
+else:  
+  f = feed.Feed(send=False, receive=True)
+  while True:
+    msg, seq, drop, m = f.recv()
+    print "%08d %1s: %s" % (seq, "*" if drop else " ", m)
