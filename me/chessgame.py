4f69e906174b4a761584f2162cfbf55c2ad2541d
Michael Schubmehl <michael.schubmehl@gmail.com>
2014-05-06 20:51:20 -0500

    chessgame: add chess message and chess game playback messages

---
+++ me/chessgame.py
@@ -0,0 +1,67 @@
+class ChessMessage:
+    pass
+
+class ChessNewGameMessage(ChessMessage):
+    code = "CN"
+    def __init__(self, gameId):
+        self.gameId  = gameId
+
+    @classmethod
+    def fromstr(cls, s):
+        msgType, gameId = s.split(",")
+        assert msgType == cls.code
+        return cls(gameId)
+
+    def __str__(self):
+        return "%s,%s" % (self.code, self.gameId)
+
+class ChessMoveMessage(ChessMessage):
+    code = "CM"
+    def __init__(self, gameId, move, history):
+        self.gameId  = gameId
+        self.move    = move
+        self.history = history
+
+    @classmethod
+    def fromstr(cls, s):
+        msgType, gameId, move, history = s.split(",")
+        assert msgType == cls.code
+        history = history.split(" ")
+        return cls(gameId, move, history)
+
+    def __str__(self):
+        return "%s,%s,%s,%s" % (self.code, self.gameId, self.move, " ".join(self.history))
+
+
+class ChessResultMessage(ChessMessage):
+    code = "CR"
+    def __init__(self, gameId, result):
+        self.gameId  = gameId
+        self.result  = result
+
+    def whiteWins(self):
+        return self.result == "1-0"
+
+    @classmethod
+    def fromstr(cls, s):
+        msgType, gameId, result = s.split(",")
+        assert msgType == cls.code
+        return cls(gameId, result)
+
+    def __str__(self):
+        return "%s,%s,%s" % (self.code, self.gameId, self.result)
+
+
+class ChessGame:
+    def __init__(self, gameId, moves, result):
+        self.gameId = gameId
+        self.moves  = moves
+        self.result = result
+
+    def messages(self):
+        yield ChessNewGameMessage(self.gameId)
+        for i in xrange(len(self.moves)):
+            yield ChessMoveMessage(self.gameId, self.moves[i], self.moves[:(i+1)])
+
+        yield ChessResultMessage(self.gameId, self.result)
+
