cef32aacd7a7bee861090d7c56aca2cb2c49ef46
Michael Schubmehl <michael.schubmehl@gmail.com>
2014-05-12 23:52:14 -0500

    me: read arbitrary chess games from first argument file

--- me/me.py
+++ me/me.py
@@ -13,11 +13,6 @@ import chessgame
 
 from log import log
 
-game = chessgame.ChessGame(gameId = 150, result = "1/2-1/2", moves="e4 c5 Nf3 e6 d4 cxd4 Nxd4 a6 Bd3 Nf6 O-O Qc7 Qe2 d6 c4 g6 Nc3 Bg7 Nf3 O-O Bf4 Nc6 Rac1 e5 Bg5 h6 Be3 Bg4 Nd5 Qd8 h3 Nxd5 cxd5 Nd4 Bxd4 Bxf3 Qxf3 exd4 Rc4 Rc8 Rfc1 Rxc4 Rxc4 h5 Qd1 Be5 Qc1 Qf6 Rc7 Rb8 a4 Kg7 b4 h4 Kf1 Bf4 Qd1 Qd8 Rc4 Rc8 a5 Rxc4 Bxc4 Qf6 Be2 Be5 Bf3 Qd8 Qc2 b6 axb6 Qxb6 Qc4 d3".split(" "))
-n = 0
-speed = 1
-nextMove = time.time() + speed
-
 class RecoveryBuilder:
     def __init__(self, b, f, freq=5):
         self.b = b
@@ -51,7 +46,15 @@ class RecoveryBuilder:
             return recoveryMessageList
 
 if __name__ == "__main__":
-    pnlFilename = sys.argv[1]
+    pnlFilename = sys.argv[2]
+
+    gamelines = open(sys.argv[1], 'r').readlines()
+
+    speed = 1
+    nextMove = time.time() + speed
+    ##game = chessgame.ChessGame(gameId = 150, result = "1/2-1/2", moves="e4 c5 Nf3 e6 d4 cxd4 Nxd4 a6 Bd3 Nf6 O-O Qc7 Qe2 d6 c4 g6 Nc3 Bg7 Nf3 O-O Bf4 Nc6 Rac1 e5 Bg5 h6 Be3 Bg4 Nd5 Qd8 h3 Nxd5 cxd5 Nd4 Bxd4 Bxf3 Qxf3 exd4 Rc4 Rc8 Rfc1 Rxc4 Rxc4 h5 Qd1 Be5 Qc1 Qf6 Rc7 Rb8 a4 Kg7 b4 h4 Kf1 Bf4 Qd1 Qd8 Rc4 Rc8 a5 Rxc4 Bxc4 Qf6 Be2 Be5 Bf3 Qd8 Qc2 b6 axb6 Qxb6 Qc4 d3".split(" "))
+    game = chessgame.ChessGame.fromstr(gamelines.pop(0))
+    n = 0
 
     debugFeedBook = True
 
@@ -89,7 +92,7 @@ if __name__ == "__main__":
                 time.sleep(5)
 
                 ## TODO: load new game
-                game = chessgame.ChessGame(gameId = str(int(game.gameId)+1), result = "1/2-1/2", moves="e4 c5 Nf3 e6 d4 cxd4 Nxd4 a6 Bd3 Nf6 O-O Qc7 Qe2 d6 c4 g6 Nc3 Bg7 Nf3 O-O Bf4 Nc6 Rac1 e5 Bg5 h6 Be3 Bg4 Nd5 Qd8 h3 Nxd5 cxd5 Nd4 Bxd4 Bxf3 Qxf3 exd4 Rc4 Rc8 Rfc1 Rxc4 Rxc4 h5 Qd1 Be5 Qc1 Qf6 Rc7 Rb8 a4 Kg7 b4 h4 Kf1 Bf4 Qd1 Qd8 Rc4 Rc8 a5 Rxc4 Bxc4 Qf6 Be2 Be5 Bf3 Qd8 Qc2 b6 axb6 Qxb6 Qc4 d3".split(" "))
+                game = chessgame.ChessGame.fromstr(gamelines.pop(0))
                 n = 0
 
                 newMsg = game.newMessage()
