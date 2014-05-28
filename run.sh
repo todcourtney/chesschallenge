7b024448748d881c52eba69847155bd5a6d8f4c4
Michael Schubmehl <michael.schubmehl@gmail.com>
2014-05-28 00:06:42 -0500

    run: notes on how to run everything

---
+++ run.sh
@@ -0,0 +1,12 @@
+## matching engine
+PYTHONPATH=common python me/me.py data/onelinegames/www.chessgames.com.csv /tmp/pnl.csv
+## pnl
+export PYTHONPATH=common; watch python me/pnl.py /tmp/pnl.csv
+## gui
+while true; do PYTHONPATH=ChessBoard:common:me python gui/gui.py /tmp/pnl.csv; sleep 1; done
+
+## strategies (python)
+PATH=/home/mschubmehl/code/chess/stockfish-dd-src/src_c11:$PATH PYTHONPATH=me:ChessBoard:common python strat/examples.py SCO SCS SCM SIMM M2M
+
+## strategies (c++)
+PATH=/home/mschubmehl/code/chess/stockfish-dd-src/src_c11:$PATH PYTHONPATH=me:ChessBoard:common:c++/swig python strat/run-cstrategy.py -s StockfishStrategy -i CSF
