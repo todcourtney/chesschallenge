0703b5a4bbcd6e7e06b630ec76fedc36f9959b68
Tod Courtney <tcourtney@jumptrading.com>
2015-05-06 17:58:57 -0500

    add PYTHONPATH to run.sh, check for NULL value from getPnl due to startup delays

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 ## matching engine
-PYTHONPATH=common python me/me.py <(shuf data/onelinegames/www.chessgames.com.csv) /tmp/pnl.csv
+PYTHONPATH=common:$PYTHONPATH; python me/me.py <(shuf data/onelinegames/www.chessgames.com.csv) /tmp/pnl.csv
 ## pnl
-export PYTHONPATH=common; watch python me/pnl.py /tmp/pnl.csv
+export PYTHONPATH=common:$PYTHONPATH; watch python me/pnl.py /tmp/pnl.csv
 ## gui
-while true; do PYTHONPATH=ChessBoard:common:me python gui/gui.py /tmp/pnl.csv; sleep 1; done
+while true; do PYTHONPATH=ChessBoard:common:me:$PYTHONPATH python gui/gui.py /tmp/pnl.csv; sleep 1; done
 
 ## strategies (python)
-PATH=/home/mschubmehl/code/chess/stockfish-dd-src/src_c11:$PATH PYTHONPATH=me:ChessBoard:common python strat/examples.py SCO SCS SCM SIMM M2M
+PATH=stockfish-dd-src/src:$PATH PYTHONPATH=me:ChessBoard:common:strat:$PYTHONPATH python strat/examples.py SCO SCS SCM SIMM M2M
 
 ## strategies (c++)
-PATH=/home/mschubmehl/code/chess/stockfish-dd-src/src_c11:$PATH PYTHONPATH=me:ChessBoard:common:c++/swig python strat/run-cstrategy.py -s StockfishStrategy -i CSF
+PATH=stockfish-dd-src/src:$PATH PYTHONPATH=me:ChessBoard:common:c++/swig:$PYTHOHNP python strat/run-cstrategy.py -s StockfishStrategy -i CSF
